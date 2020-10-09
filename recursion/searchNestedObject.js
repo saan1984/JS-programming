af4f0daad9781aa09954bf1bdd47bf51bea57ced
sandeeppatel <sandeeppateltech@gmail.com>
2020-10-09 10:41:05 +0530

    changes

---
+++ recursion/searchNestedObject.js
@@ -0,0 +1,8 @@
+//Write a function called contains that searches for a value in a nested object.
+// It returns true if the object contains that value.
+// Example, nestedObject={ a: {b: c:{d:44}}} contains(nestedObject, 44); // true
+
+
+function contains(obj) {
+    if(obj)
+}
